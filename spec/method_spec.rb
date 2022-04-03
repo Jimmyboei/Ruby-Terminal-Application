7d6e2213b2998a9ec797477b65e1694991546581
jimmyboei <hellocage@hotmail.com>
2022-04-03 21:54:53 +1000

    Finish tests

--- spec/method_spec.rb
+++ spec/method_spec.rb
@@ -12,3 +12,17 @@ describe 'check_password' do
         expect { check_password(user, "2234") }.to raise_error(InvalidPasswordError)
     end
 end
+
+describe 'reset_calorie_progress' do
+    it 'should reset progress to zero' do
+        user = { name: "Jim", password: "1234", goal: "1500", progress: 1400 }
+        reset_calorie_progress(user)
+        expect(user[:progress]).to eq 0
+    end
+    it 'should reset clear intake and workout activities' do
+        user = { name: "Jim", password: "1234", goal: "1500", progress: 1400 }
+        reset_calorie_progress(user)
+        expect(user[:intakes]).to be_empty
+        expect(user[:workouts]).to be_empty
+    end
+end
